c77ccbcdd7d548e6c3fe6563589c0f733c9c5bd1
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2018-11-11 23:35:08 +0100

    :sparkles: Added player controller script

---
+++ SlingshotController/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[Header ("Movement")]
+	public float forceToAdd = 5f;
+	private bool addedForce = false;
+
+	[Header ("Ground check")]
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private LayerMask whatIsGround;
+
+	private bool grounded;
+	const float groundedRadius = .15f;
+
+	private Rigidbody2D rb;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		grounded = false;
+
+		Collider2D[] groundColliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+		for (int i = 0; i < groundColliders.Length; i++) {
+			if (groundColliders[i].gameObject != gameObject) {
+				grounded = true;
+			}
+		}
+	}
+
+	public void AddForce(Vector2 force) {
+		if (!grounded) {
+			return;
+		}
+
+		Vector3 targetVelocity = new Vector2(force.x * forceToAdd, force.y * forceToAdd);
+
+		rb.velocity = targetVelocity;
+	}
+	
+}
